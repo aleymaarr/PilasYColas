120a784ca3f55309a71accd1eb5433b9789b4a1b
aleymarr <aleyba2002@gmail.com>
2023-05-04 18:11:36 +0100

    Feat(cajero):Creamos el menu para la rama cajero

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import com.hiperdino.entidades.Cajero;
+import com.hiperdino.entidades.Cliente;
+import com.hiperdino.utilidades.GeneradorAleatorio;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Cajero cajero  = new Cajero();
+        Cliente cliente;
+        GeneradorAleatorio generadorAleatorio = new GeneradorAleatorio();
+        Scanner scanner = new Scanner(System.in);
+
     }
 }
