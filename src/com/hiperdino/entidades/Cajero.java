a84a6737907307508ef6c5b10349d27251c65aa2
aleymarr <aleyba2002@gmail.com>
2023-05-04 17:57:53 +0100

    Feat(cajero):Creando los atributos y los contructores para la clase cajero

--- src/com/hiperdino/entidades/Cajero.java
+++ src/com/hiperdino/entidades/Cajero.java
@@ -1,4 +1,33 @@
 package com.hiperdino.entidades;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Cajero {
+
+    private static int contadorCajas = 0;
+    private final int numeroCaja;
+    private final Queue<Cliente> filaClientes;
+    private boolean cajaAbierta;
+
+    private static int contadorCajas = 0;
+    private final int numeroCaja;
+    private final Queue<Cliente> filaClientes;
+    private boolean cajaAbierta;
+
+    public Cajero() {
+        contadorCajas++;
+        this.numeroCaja = contadorCajas;
+        this.filaClientes = new LinkedList<>();
+        this.cajaAbierta = false;
+    }
+
+    public Cajero() {
+        contadorCajas++;
+        this.numeroCaja = contadorCajas;
+        this.filaClientes = new LinkedList<>();
+        this.cajaAbierta = false;
+    }
+
+
 }
