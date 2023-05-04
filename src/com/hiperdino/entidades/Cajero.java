44a14168893aed626e6e50b4d0322e99786b4826
aleymarr <aleyba2002@gmail.com>
2023-05-04 18:01:15 +0100

    Feat(cajero):Creacion de los metodos para la clase cajero

--- src/com/hiperdino/entidades/Cajero.java
+++ src/com/hiperdino/entidades/Cajero.java
@@ -18,7 +18,54 @@ public class Cajero {
         this.cajaAbierta = false;
     }
 
+    public void abrirCaja() {
+        cajaAbierta = true;
+        System.out.println("Caja abierta.");
+    }
+
+    public void cerrarCaja() {
+        cajaAbierta = false;
+        filaClientes.clear();
+        System.out.println("Caja cerrada y cola de clientes vaciada.");
+    }
+
+    public void abrirCaja() {
+        cajaAbierta = true;
+        System.out.println("Caja abierta.");
+    }
+
+    public void cerrarCaja() {
+        cajaAbierta = false;
+        filaClientes.clear();
+        System.out.println("Caja cerrada y cola de clientes vaciada.");
+    }
+
+    public boolean estadoCaja() {
+        return cajaAbierta;
+    }
+
+    public void agregarClienteACola(Cliente cliente) {
+        filaClientes.add(cliente);
+    }
 
 
+    public Cliente atenderCliente() {
+        return filaClientes.poll();
+    }
+
+    public int getTotalClientes() {
+        return filaClientes.size();
+    }
+
+    public void mostrarClientesPendientes() {
+        if (!filaClientes.isEmpty()) {
+            System.out.println("Clientes pendientes en la cola:");
+            for (Cliente cliente : filaClientes) {
+                System.out.println(cliente.getNombre());
+            }
+        } else {
+            System.out.println("No hay clientes pendientes en la cola.");
+        }
+    }
 
 }
