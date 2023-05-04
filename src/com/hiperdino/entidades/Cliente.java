2a67445717e82bb6c6cdfe002e24098fb828daf2
aleymarr <aleyba2002@gmail.com>
2023-05-04 18:04:44 +0100

    Feat(cajero):Creacion de los atributos y contructores en la clase cliente

--- src/com/hiperdino/entidades/Cliente.java
+++ src/com/hiperdino/entidades/Cliente.java
@@ -1,4 +1,23 @@
 package com.hiperdino.entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
+
+    private final String nombre;
+    private final List<String> cestaCompra;
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+        this.cestaCompra = new ArrayList<>();
+    }
+
+    private final String nombre;
+    private final List<String> cestaCompra;
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+        this.cestaCompra = new ArrayList<>();
+    }
 }
